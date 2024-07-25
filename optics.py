c89e49f3b9dce88d3eeca0beb48e683214e0dec3
NEKOOAO <60414oscar@gmail.com>
2024-07-25 15:08:34 +0800

    add readfile to clustering

--- optics.py
+++ optics.py
@@ -2,11 +2,18 @@ from sklearn.cluster import OPTICS
 import numpy as np
 class optic():
     def __init__(self) :
-        self.opt= OPTICS(min_samples=3, metric="precomputed")
+        self.opt= OPTICS(min_samples=2, metric="precomputed")
     def clustering(self,graph):
         arr = np.array(graph)
         cluster = self.opt.fit(arr)
         print(max(cluster.labels_))
         print (cluster.labels_)
-        
-        
+
+dis_graph=[]
+path = 'test'
+f = open(path, 'r')
+lis = f.readlines()
+for i in lis :
+    dis_graph.append(i.split())
+f.close()
+optic().clustering(dis_graph)
